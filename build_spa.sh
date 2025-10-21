5485b9f61f67a4072d145d8b132d0d31c6b7db67
OlZe <oliverzeidler97@gmail.com>
2025-10-21 22:32:09 +0200

    build page

---
+++ build_spa.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'building...'
+rm -rf dist
+gleam run -m lustre/dev build app --minify
+
+echo 'preparing output dir...'
+rm -rf built_spa
+cp -r dist built_spa
+mv built_spa/assets/* built_spa/
+rm -r built_spa/assets
